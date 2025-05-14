ccb0c284b1bcb50bd8ab98ef9383f7db56d27dc0
pford68 <dev@philipford.com>
2025-05-14 11:44:49 -0400

    Using SQLAlchemy

--- configuration.py
+++ configuration.py
@@ -4,28 +4,28 @@ from dotenv import load_dotenv
 load_dotenv()
 
 try:
-    os.environ["FINQ_DB_USER"]
+    os.environ["RDS_USER"]
 except KeyError as e:
     print("Set the required environment variables. See the documentation for more details.")
 
 '''Configuration class
 
 Requires the following environment variables:
-* FINQ_DB_USER
-* FINQ_DB_PASSWORD
-* FINQ_DB_HOST
-* FINQ_DB_NAME
+* RDS_USER
+* RDS_PASSWORD
+* RDS_HOST
+* RDS_NAME
 
 The following environment variables are optional:
-* FINQ_DB_PORT:  defaults to 3306
+* RDS_PORT:  defaults to 3306
 '''
 class Configuration:
-    TYPE = os.getenv('FINQ_DB_TYPE')
-    USER = os.getenv('FINQ_DB_USER')
-    PASSWORD = os.getenv('FINQ_DB_PWD')
-    HOST = os.getenv('FINQ_DB_HOST')
-    PORT = os.getenv('FINQ_DB_PORT', 3306)
-    DATABASE = os.getenv('FINQ_DB_NAME')
+    TYPE = os.getenv('RDS_TYPE')
+    USER = os.getenv('RDS_USER')
+    PASSWORD = os.getenv('RDS_PWD')
+    HOST = os.getenv('RDS_HOST')
+    PORT = os.getenv('RDS_PORT', 3306)
+    DATABASE = os.getenv('RDS_DB_NAME')
     RAISE_ON_WARNINGS = True
 
     def __init__(self, **kwargs):
